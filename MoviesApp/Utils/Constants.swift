e5860f7f5af564549368647754cf74dfa9a1dc08
akidon0000 <akidon0000@gmail.com>
2024-04-30 11:19:31 +0900

    Constantsは値は入れずに初期状態はGitに追加した方が、別の人がビルドした際にエラーが起きない

---
+++ MoviesApp/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MoviesApp
+//
+//  Created by Akihiro Matsuyama on 2024/04/30.
+//  Copyright © 2024 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = ""
+}
